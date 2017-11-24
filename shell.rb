34c7eaf62cf6f25df84bbb8efd875733d658fbe2
Tom Mestdagh <tmestdagh@digidentity.com>
2017-11-24 11:13:05 +0100

    Added autocompletion support for directories and previously used commands.

--- shell.rb
+++ shell.rb
@@ -1,10 +1,19 @@
+# http://www.rubyguides.com/2016/07/writing-a-shell-in-ruby/
 require 'readline'
 
 while input = Readline.readline("> ", true)
   break if input == "exit"
   puts Readline::HISTORY.to_a if input == "hist"
 
-  comp = proc { |s| Readline::HISTORY.grep(/^#{Regexp.escape(s)}/) }
+  comp = proc do |s|
+    dir_list = Dir.glob("#{s}*")
+
+    if dir_list.size > 0
+      dir_list
+    else
+      Readline::HISTORY.grep(/^#{Regexp.escape(s)}/)
+    end
+  end
 
   Readline.completion_append_character = " "
   Readline.completion_proc = comp
